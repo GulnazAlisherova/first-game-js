1c9b6bc321575abb8b710b7c3e331ea823fd7dec
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-31 19:43:42 +0600

    inside main function "jump" added if with several methods

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump() {
+  if(character.classList !="animate"){
+    character.classList.add("animate");
+  }
+
   character.classList.add("animate");
   setTimeout(function () {
     character.classList.remove("animate");
