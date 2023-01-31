e39c4d96c063591c2fdfb53c3c8d873a666d66d1
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-31 19:52:21 +0600

    formated and added alert for if

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump() {
-  if(character.classList !="animate"){
+  if (character.classList != "animate") {
     character.classList.add("animate");
   }
   character.classList.add("animate");
@@ -10,7 +10,12 @@ function jump() {
     character.classList.remove("animate");
   }, 500);
 }
-var checkHead = setInterval(function(){
-  var characterTop = 
-  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-},10)
+var checkHead = setInterval(function () {
+  var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (blockLeft < 20 && blockLeft > 0 %% characterTop >= 130) {
+    alert("u lose.")
+  }
+}, 10)
